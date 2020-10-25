909251754ea83009c683cbff581cdc2ac7f53543
Romanchikus <13ternopil@gmail.com>
2020-10-25 12:46:59 +0200

    openapi - 3.0, yml-response, norm_name_func

--- handler.py
+++ handler.py
@@ -5,25 +5,28 @@ import asyncio
 
 
 def hello(event, context):
-    body={}
+    response={}
     try:
         urls = event['website']
     except:
-        body = {
+        response = {
             "Error": 'Please choose needed websites'
         }
     else:
-        body =asyncio.run(get_dict_timeurl(urls))
+        response =asyncio.run(get_time_for_urls(urls))
     
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
     return response
-    
 
-async def site_response(url="https://www.youtube.com/"):
+async def get_time_for_urls(urls):
+    
+    start = time.time()
+    tasks = [asyncio.create_task(
+        response_from_site(url)) for url in urls]
+    time_from_tasks = await asyncio.wait(tasks)
+
+    return get_time_response(urls, time_from_tasks[0])    
+
+async def response_from_site(url="https://www.youtube.com/"):
     t1 = time.time()
     timeout = aiohttp.ClientTimeout(total=3)
     async with aiohttp.ClientSession() as session:
@@ -36,19 +39,13 @@ async def site_response(url="https://www.youtube.com/"):
             return getattr(e, 'message', 'Server not available'),url
     return round((time.time()-t1),2),url
 
-async def get_dict_timeurl(urls):
-    start = time.time()
-    tasks = [asyncio.create_task(
-        site_response(url)) for url in urls]
-    time_task = await asyncio.wait(tasks)
-    dict_of_response = get_time_response(urls, time_task[0])
-
-    return dict_of_response
     
 
-def get_time_response(url,time_task):
+def get_time_response(url,time_from_tasks):
     dict_of_response= {}
-    for i in time_task:
-        time_response,url = i.result()
+    
+    for time_task in time_from_tasks:
+        time_response,url = time_task.result()
         dict_of_response[url]=time_response
+        
     return dict_of_response
